7e27c7cbdf83d59b84b65a669b3acea9c46fe283
Ahmedsaif78 <as4675875@gmail.com>
2024-02-09 23:55:13 +0300

    first

---
+++ android/app/src/main/kotlin/com/example/ruotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ruotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import '/app_ly/app.dart';
+void main() {
+  runApp(  MyApp());
+}
